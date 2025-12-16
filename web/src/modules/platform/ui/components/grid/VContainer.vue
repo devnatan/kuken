b4c2017e273d538305fa30c556ef8a047da7450a
Natan <natanvnascimento@gmail.com>
2025-12-16 14:20:54 -0300

    Refactor grid container styles by moving `.container` logic into VContainer component with scoped styles

--- web/src/modules/platform/ui/components/grid/VContainer.vue
+++ web/src/modules/platform/ui/components/grid/VContainer.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="container">
+  <div :class="$style.container">
     <slot />
   </div>
 </template>
+<style lang="scss" module>
+.root {
+  position: relative;
+  width: 100%;
+  max-width: calc(1170px * 1.6);
+  margin: 0 auto;
+  box-sizing: border-box;
+
+  &:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+}
+</style>
