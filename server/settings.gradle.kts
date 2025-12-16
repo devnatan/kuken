05568be1eb740c96ae1ead854fa95bdb6acbca8d
Natan <natanvnascimento@gmail.com>
2025-12-16 18:37:16 -0300

    chore: add docker-kotlin repository

--- server/settings.gradle.kts
+++ server/settings.gradle.kts
@@ -10,5 +10,16 @@ pluginManagement {
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+        maven("https://maven.pkg.github.com/devnatan/docker-kotlin") {
+            credentials {
+                username = providers.environmentVariable("GITHUB_ACTOR")
+                    .orElse(providers.gradleProperty("githubActor"))
+                    .get()
+
+                password = providers.environmentVariable("GITHUB_TOKEN")
+                    .orElse(providers.gradleProperty("githubToken"))
+                    .get()
+            }
+        }
     }
 }
