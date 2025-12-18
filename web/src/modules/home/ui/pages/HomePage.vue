f8c65a611163b9672423f35f66b0bc0cbf21aa67
Natan <natanvnascimento@gmail.com>
2025-12-18 11:14:19 -0300

    refactor(web): simplify `HomePage` layout by replacing static components with `router-view` and `VContainer`

--- web/src/modules/home/ui/pages/HomePage.vue
+++ web/src/modules/home/ui/pages/HomePage.vue
@@ -1,18 +1,11 @@
 <template>
-    <div class="container">
-        <Breadcrumb />
-        <VTitle> Hello {{ account.email }} </VTitle>
-        <VButton to="setup"> Go to setup </VButton>
-    </div>
+    <VContainer class="container">
+        <router-view />
+    </VContainer>
 </template>
 
 <script setup lang="ts">
-import Breadcrumb from "@/modules/platform/ui/components/Breadcrumb.vue"
-import { useAccount } from "@/modules/accounts/accounts.store.ts"
-import VButton from "@/modules/platform/ui/components/button/VButton.vue"
-import VTitle from "@/modules/platform/ui/components/typography/VTitle.vue"
-
-const account = useAccount()
+import VContainer from "@/modules/platform/ui/components/grid/VContainer.vue"
 </script>
 
 <style scoped lang="scss">
