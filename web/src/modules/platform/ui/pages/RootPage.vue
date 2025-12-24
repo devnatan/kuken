aa7a2f3bafe2adacc4629c160004b72285f1b8d4
Natan <natanvnascimento@gmail.com>
2025-12-24 10:11:28 -0300

    refactor(web/platform): replace `useRouter` with `useRoute`

--- web/src/modules/platform/ui/pages/RootPage.vue
+++ web/src/modules/platform/ui/pages/RootPage.vue
@@ -4,10 +4,10 @@
     </component>
 </template>
 <script setup lang="ts">
-import { useRouter } from "vue-router"
+import {useRoute} from "vue-router"
 import RootLayout from "@/modules/platform/ui/layouts/RootLayout.vue"
-import { computed } from "vue"
+import {computed} from "vue"
 
-const router = useRouter()
-const layout = computed(() => router.currentRoute.value.meta.layout ?? RootLayout)
+const currentRoute = useRoute()
+const layout = computed(() => currentRoute.meta.layout ?? RootLayout)
 </script>
