6c1db58ce605fab3fbe2fa1cd5b408fddee28cfa
Natan <natanvnascimento@gmail.com>
2025-12-28 18:40:56 -0300

    refactor(server/http): simplify Errors definitions by removing unused error codes

--- server/src/main/kotlin/gg/kuken/http/Errors.kt
+++ server/src/main/kotlin/gg/kuken/http/Errors.kt
@@ -18,32 +18,15 @@ data class HttpError(
     val details: String?,
 ) {
     companion object {
-        val Conflict = error(0, "Conflict")
+        val InvalidAccessToken = error(2001, "Invalid or missing access token")
+        val InsufficientPermissions = error(2002, "Insufficient permissions to access this resource")
+
         val NotFound = error(0, "Not Found")
-        val Generic: (String) -> HttpError = { message -> error(0, message) }
         val UnknownAccount = error(1001, "Unknown account")
         val UnknownUnit = error(1002, "Unknown unit")
-        val UnknownInstance = error(1003, "Unknown instance")
-        val UnknownFSBucket = error(1004, "Unknown file system bucket")
-        val UnknownFSFile = error(1005, "Unknown file")
-        val InstanceRuntimeNotAvailable = error(1006, "Instance runtime not available")
-        val ResourceNotAccessible = error(1007, "Resource not accessible")
-        val FileIsNotDirectory = error(1008, "File is not a directory")
-        val RequestedResourceIsNotAFile = error(1009, "The requested resource is not a file")
-        val UnavailableFileSystem = error(1010, "Unavailable file system")
         val UnknownBlueprint = error(1011, "Unknown blueprint")
-        val BlueprintParse: (String) -> HttpError =
-            { error(1012, "Failed to parse blueprint file", it) }
-        val InvalidAccessToken = error(2001, "Invalid or missing access token")
-        val AccountInvalidCredentials = error(2002, "Invalid account credentials")
-        val AccountLoginConflict =
-            error(
-                2003,
-                "An account with that username or email already exists",
-            )
-        val InvalidInstanceUpdateCode = error(3001, "Invalid instance update code")
+
         val FailedToParseRequestBody = error(3002, "Failed to handle request")
-        val InvalidRequestBody = error(3003, "Invalid request body")
 
         fun error(
             code: Int,
