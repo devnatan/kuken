42130d7b7d1455ea77cd828d2c7411ef78ea1004
Natan <natanvnascimento@gmail.com>
2026-01-03 09:52:34 -0300

    refactor(server/docker): remove `DockerEventDispatcher` unused coroutine scopes

--- server/src/main/kotlin/gg/kuken/core/docker/DockerEventDispatcher.kt
+++ server/src/main/kotlin/gg/kuken/core/docker/DockerEventDispatcher.kt
@@ -2,13 +2,9 @@ package gg.kuken.core.docker
 
 import gg.kuken.core.EventDispatcher
 import gg.kuken.core.EventDispatcherImpl
-import gg.kuken.feature.instance.event.InstanceStartedEvent
+import gg.kuken.feature.instance.event.InstanceEvent
 import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.supervisorScope
 import me.devnatan.dockerkt.DockerClient
 import me.devnatan.dockerkt.models.system.Event
 import me.devnatan.dockerkt.models.system.EventActor
@@ -31,14 +27,15 @@ class DockerEventDispatcher(
         }
     }
 
-    private suspend fun interceptDockerEvent(event: Event) = supervisorScope {
+    private suspend fun interceptDockerEvent(event: Event) {
         when (event.action) {
-            "start" -> launch(start = CoroutineStart.ATOMIC) { dispatch(InstanceStartedEvent(event.actor.getInstanceId())) }
+            "start" -> dispatch(InstanceEvent.InstanceStartedEvent(event.actor.getInstanceId()))
             else -> Unit
         }
     }
 
-    private fun EventActor.getInstanceId(): Uuid = requireNotNull(attributes["gg.kuken.instance.id"]?.let(Uuid.Companion::parse)) {
-        "Instance id not found in event actor attributes: $attributes"
-    }
+    private fun EventActor.getInstanceId(): Uuid =
+        requireNotNull(attributes["gg.kuken.instance.id"]?.let(Uuid.Companion::parse)) {
+            "Instance id not found in event actor attributes: $attributes"
+        }
 }
