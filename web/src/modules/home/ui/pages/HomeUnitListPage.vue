cf957810357e9f24b274bcee9bf1a9a95375da42
Natan <natanvnascimento@gmail.com>
2026-01-05 12:40:19 -0300

    feat(web): enhance HomeUnitListPage with header, server count, and improved unit display

--- web/src/modules/home/ui/pages/HomeUnitListPage.vue
+++ web/src/modules/home/ui/pages/HomeUnitListPage.vue
@@ -1,15 +1,24 @@
 <template>
+    <VTitle>Welcome</VTitle>
+    <div class="header">
+        <h4>Your Server List</h4>
+        <span class="count" v-text="state.units.length" />
+    </div>
     <VContainer class="container">
-        <Resource :resource="unitsService.listUnits" @loaded="(data) => (state.units = data)">
-            <ul>
-                <router-link
-                    :to="{ name: 'instance.console', params: { instanceId: unit.instance } }"
-                    v-for="unit in state.units"
-                    :key="unit.id"
-                >
-                    Unit: {{ unit.name }}
-                </router-link>
-            </ul>
+        <Resource
+            :resource="unitsService.listUnits"
+            @loaded="(data: Unit[]) => (state.units = data)"
+            class="server-list"
+        >
+            <router-link
+                :to="{ name: 'instance.console', params: { instanceId: unit.instance.id } }"
+                v-for="unit in state.units"
+                :key="unit.id"
+                class="server-list-item"
+            >
+                <ProgressiveImage :src="unit.instance.blueprint.iconUrl!" />
+                Unit: {{ unit.name }}
+            </router-link>
         </Resource>
         <VButton variant="primary" to="blueprints"> Go to blueprints </VButton>
     </VContainer>
@@ -22,6 +31,8 @@ import { reactive } from "vue"
 import type { Unit } from "@/modules/units/api/models/unit.model.ts"
 import Resource from "@/modules/platform/ui/components/Resource.vue"
 import unitsService from "@/modules/units/api/services/units.service.ts"
+import VTitle from "@/modules/platform/ui/components/typography/VTitle.vue"
+import { ProgressiveImage } from "vue-progressive-image"
 
 let state = reactive({ units: [] as Unit[] })
 </script>
@@ -31,5 +42,40 @@ let state = reactive({ units: [] as Unit[] })
     padding: 48px;
     display: flex;
     flex-direction: column;
+}
+
+.header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 12px;
+
+    h4 {
+        margin: 0;
+    }
+
+    .count {
+        background-color: var(--kt-content-primary);
+        width: 20px;
+        height: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        user-select: none;
+        color: #fff;
+        font-family: var(--kt-headline-font), serif;
+        font-size: 14px;
+        font-weight: bold;
+        border-radius: 4px;
+    }
+}
+
+.server-list {
+    display: flex;
+    flex-direction: column;
+
+    .server-list-item {
+    }
 }
 </style>
