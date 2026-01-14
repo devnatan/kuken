f6e51dc33e717eeb7cc66cf816754ef9c5912923
Natan <natanvnascimento@gmail.com>
2026-01-14 02:43:00 -0300

    Update blueprint spec model

--- web/src/modules/blueprints/api/models/blueprint.model.ts
+++ web/src/modules/blueprints/api/models/blueprint.model.ts
@@ -4,15 +4,43 @@ export type Blueprint = {
 }
 
 export type BlueprintSpec = {
+    source: string
+    descriptor: BlueprintDescriptor
+    build: BlueprintBuild
+    inputs: { [name: string]: BlueprintBuildInput }
+    assets: {
+        icon?: BlueprintAsset
+    }
+}
+
+export type BlueprintAsset = {
+    type: "image"
+    source: string
+}
+
+export function resolveBlueprintSource(source: string): string {
+    return source.substring(source.indexOf("://") + 3, source.length)
+}
+
+export type BlueprintDescriptor = {
     name: string
     version: string
-    remote: BlueprintRemote
 }
 
-export type BlueprintRemote = {
-    assets: BlueprintRemoteAssets
+export type BlueprintBuild = {
+    env: { [key: string]: BlueprintProperty }
 }
 
-export type BlueprintRemoteAssets = {
-    iconUrl: string
+export type BlueprintProperty = {
+    type: "placeholder" | "input" | "env"
+    expr?: string
+    name?: string
+}
+
+export type BlueprintBuildInput = {
+    type: "text" | "port"
+    label: string
+    placeholder?: string
+    sensitive?: boolean
+    default?: any
 }
