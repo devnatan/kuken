17b90118dbc37d03539d18da5a7d09b627840a78
Natan <natanvnascimento@gmail.com>
2026-01-14 02:42:47 -0300

    Create BlueprintInputText

---
+++ web/src/modules/blueprints/ui/components/BlueprintInputText.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import VInput from "@/modules/platform/ui/components/form/VInput.vue"
+import VLabel from "@/modules/platform/ui/components/form/VLabel.vue"
+import VFieldSet from "@/modules/platform/ui/components/form/VFieldSet.vue"
+import type { BlueprintBuildInput } from "@/modules/blueprints/api/models/blueprint.model.ts"
+
+const props = defineProps<Partial<BlueprintBuildInput>>()
+const model = defineModel()
+</script>
+
+<template>
+    <VFieldSet>
+        <VLabel>
+            {{ props.label }}
+            <VInput
+                ref="input"
+                :placeholder="props.placeholder"
+                :type="props.sensitive ? 'password' : 'text'"
+                :value="model"
+                autoFocus
+                required="true"
+            />
+        </VLabel>
+    </VFieldSet>
+</template>
+
+<style lang="scss" scoped></style>
