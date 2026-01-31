d05203fbcff934dc3a24c36665fd90331db75441
Natan <natanvnascimento@gmail.com>
2026-01-31 20:51:03 -0300

    Update legacy vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,21 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig, splitVendorChunkPlugin } from "vite"
+import vue from "@vitejs/plugin-vue"
 // @ts-ignore
 import pkg from "./package.json"
-import { splitVendorChunkPlugin } from 'vite'
 
 process.env.VITE_APP_VERSION = pkg.version
 
 export default defineConfig({
-    plugins: [
-        splitVendorChunkPlugin(),
-        vue()
-    ],
+    plugins: [splitVendorChunkPlugin(), vue()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            '@icons': fileURLToPath(new URL('./node_modules/vue-material-design-icons', import.meta.url)),
+            "@": fileURLToPath(new URL("./src", import.meta.url))
         }
     }
 })
