80414168e6ccc715db2756b6d0e1480948bc66f8
Natan <natanvnascimento@gmail.com>
2026-02-02 02:58:49 -0300

    Render loading state properly on full height page

--- web/src/modules/platform/ui/components/LoadingState.vue
+++ web/src/modules/platform/ui/components/LoadingState.vue
@@ -1,11 +1,11 @@
 <template>
-  <div ref="root" class="loading-state" />
+  <div ref="root" class="loading" />
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from "vue"
+import { onMounted, ref, useTemplateRef } from "vue"
 import { useLoading } from "vue-loading-overlay"
 
-const root = ref(null)
+const root = useTemplateRef("root")
 const loading = useLoading({
   isFullPage: true,
   lockScroll: true
@@ -15,3 +15,14 @@ onMounted(() => {
   loading.show({ container: root?.value as unknown as HTMLElement })
 })
 </script>
+<style lang="scss" scoped>
+.loading {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
