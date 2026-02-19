bb5a51ea9e621863211d7532f4288a2594505a0f
Natan <natanvnascimento@gmail.com>
2026-02-19 03:57:30 -0300

    Enable compact output and customize chunk splitting

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -13,5 +13,35 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        compact: true,
+        assetFileNames: "[ext]/[hash][extname]",
+        chunkFileNames: "js/[hash].js",
+        manualChunks: (id) => {
+          if (id.includes("codemirror")) {
+            return `codeeditor`
+          }
+
+          if (id.includes("/locales/")) {
+            return `lang-${id}`
+          }
+
+          if (id.includes("/modules/")) {
+            return "app"
+          }
+
+          if (id.includes("/@vue/")) {
+            return `core`
+          }
+
+          if (id.includes("/node_modules/")) {
+            return `vendor`
+          }
+        }
+      }
+    }
   }
 })
